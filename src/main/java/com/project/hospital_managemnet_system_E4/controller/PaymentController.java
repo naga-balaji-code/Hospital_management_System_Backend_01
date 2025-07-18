0a50cb3411cf72d16f7fd723775a0f01b019352d
naga-balaji-code <nagabalaji1016@gmail.com>
2025-07-18 15:27:46 +0530

    do some changes on encounter and payment

--- src/main/java/com/project/hospital_managemnet_system_E4/controller/PaymentController.java
+++ src/main/java/com/project/hospital_managemnet_system_E4/controller/PaymentController.java
@@ -4,54 +4,42 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.project.hospital_managemnet_system_E4.dto.Patient;
 import com.project.hospital_managemnet_system_E4.dto.Payment;
 import com.project.hospital_managemnet_system_E4.service.PaymentService;
 import com.project.hospital_managemnet_system_E4.util.ResponseStructure;
 
 @RestController
-//@CrossOrigin(origins = "http://localhost:5176")
 @CrossOrigin(origins = "*") 
 public class PaymentController {
-	
-	@Autowired
-	PaymentService paymentService;
+    
+    @Autowired
+    PaymentService paymentService;
 
-	@PostMapping("/savePayment")
-	public ResponseEntity<ResponseStructure<Payment>> savePayment(@RequestBody Payment payment) {
-		return paymentService.savePayment(payment);
-	}
+    @PostMapping("/savePayment")
+    public ResponseEntity<ResponseStructure<Payment>> savePayment(@RequestBody Payment payment) {
+        return paymentService.savePayment(payment);
+    }
 
-	@PutMapping("/updatePaymentById")
-	public ResponseEntity<ResponseStructure<Payment>> updatePaymentById (@RequestParam int oldPaymentId,@RequestBody Payment newPayment) {
+    @PutMapping("/updatePaymentById")
+    public ResponseEntity<ResponseStructure<Payment>> updatePaymentById(@RequestParam int oldPaymentId,
+            @RequestBody Payment newPayment) {
+        return paymentService.updatePaymentById(oldPaymentId, newPayment);
+    }
 
-		return paymentService.updatePaymentById(oldPaymentId, newPayment);
-	}
+    @GetMapping("/fetchPaymentById")
+    public ResponseEntity<ResponseStructure<Payment>> fetchPaymentById(@RequestParam int paymentId) {
+        return paymentService.fetchPaymentById(paymentId);
+    }
 
-	@GetMapping("/fetchPaymentById")
-	public ResponseEntity<ResponseStructure<Payment>> fetchPaymentById(@RequestParam int paymentId) {
-		return paymentService.fetchPaymentById(paymentId);
-	}
+    @DeleteMapping("/deletePaymentById")
+    public ResponseEntity<ResponseStructure<Payment>> deletePaymentById(@RequestParam int paymentId) {
+        return paymentService.deletePaymentById(paymentId);
+    }
 
-	@DeleteMapping("/deletePaymentById")
-	public ResponseEntity<ResponseStructure<Payment>> deletePaymentById(@RequestParam int paymentId) {
-
-		return paymentService.deletePaymentById(paymentId);
-	}
-
-	@GetMapping("/fetchAllPayment")
-	public List<Payment> fetchAllPayment() {
-		return paymentService.fetchAllPayment();
-	}
-	
-
+    @GetMapping("/fetchAllPayment")
+    public List<Payment> fetchAllPayment() {
+        return paymentService.fetchAllPayment();
+    }
 }
--- src/main/java/com/project/hospital_managemnet_system_E4/dto/Payment.java
+++ src/main/java/com/project/hospital_managemnet_system_E4/dto/Payment.java
@@ -7,53 +7,72 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
 @Entity
 public class Payment {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int paymentId;
-	private String paymentType;
-	private double paymentAmount;
-	private String paymentStatus;
-	private Date paymentDate;
-	private Time paymentTime;
-	public int getPaymentId() {
-		return paymentId;
-	}
-	public void setPaymentId(int paymentId) {
-		this.paymentId = paymentId;
-	}
-	public String getPaymentType() {
-		return paymentType;
-	}
-	public void setPaymentType(String paymentType) {
-		this.paymentType = paymentType;
-	}
-	public double getPaymentAmount() {
-		return paymentAmount;
-	}
-	public void setPaymentAmount(double paymentAmount) {
-		this.paymentAmount = paymentAmount;
-	}
-	public String getPaymentStatus() {
-		return paymentStatus;
-	}
-	public void setPaymentStatus(String paymentStatus) {
-		this.paymentStatus = paymentStatus;
-	}
-	public Date getPaymentDate() {
-		return paymentDate;
-	}
-	public void setPaymentDate(Date paymentDate) {
-		this.paymentDate = paymentDate;
-	}
-	public Time getPaymentTime() {
-		return paymentTime;
-	}
-	public void setPaymentTime(Time paymentTime) {
-		this.paymentTime = paymentTime;
-	}
-	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int paymentId;
 
+    private String paymentType;
+    private double paymentAmount;
+    private String paymentStatus;
+
+    // ✅ Keep Date & Time
+    private Date paymentDate;
+    private Time paymentTime;
+
+    // ✅ Add relationship to Patient (optional)
+    @ManyToOne
+    @JoinColumn(name = "patient_id")
+    private Patient patient;
+
+    // ✅ Getters/Setters
+    public int getPaymentId() {
+        return paymentId;
+    }
+    public void setPaymentId(int paymentId) {
+        this.paymentId = paymentId;
+    }
+    public String getPaymentType() {
+        return paymentType;
+    }
+    public void setPaymentType(String paymentType) {
+        this.paymentType = paymentType;
+    }
+    public double getPaymentAmount() {
+        return paymentAmount;
+    }
+    public void setPaymentAmount(double paymentAmount) {
+        this.paymentAmount = paymentAmount;
+    }
+    public String getPaymentStatus() {
+        return paymentStatus;
+    }
+    public void setPaymentStatus(String paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+    public Date getPaymentDate() {
+        return paymentDate;
+    }
+    public void setPaymentDate(Date paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+    public Time getPaymentTime() {
+        return paymentTime;
+    }
+    
+    public void setPaymentTime(Time paymentTime) {
+        this.paymentTime = paymentTime;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
 }
